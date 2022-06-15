f9511332e2c7c5e940617f4e2bad57b160ef4765
Con Sınov <hakanunal@devtagon.com>
2022-06-15 19:45:51 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { useLocalStorage } from "./useLocalStorage"
 import './App.css';
@@ -8,25 +8,46 @@ import './App.css';
 const App = () => {
 
   const [name, setName] = useLocalStorage("name", "")
+  const [surname, setSurname] = useState("")
   const renderCount = useRef(0)
+  const inputRef = useRef(0)
 
+
+
+  const handleSubmit = () => {
+    inputRef.current.focus()
+    inputRef.current.value = "click"
+  }
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1
   })
 
+
+
   return (
-    <div className="App">
-      <input
-        type="text"
-        value={name}
-        onChange={e => setName(e.target.value)}
-      ></input>
+    <>
+      <div className="App">
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        ></input>
+      </div>
+      <div className="App">
+
+        <input
+          type="text"
+          ref={inputRef}
+          value={surname}
+          onChange={e => setSurname(e.target.value)}
+        ></input>
+        <button onClick={handleSubmit}>Submit</button>
 
 
-
-      <p>{renderCount.current}</p>
-    </div>
+        <p>{renderCount.current}</p>
+      </div>
+    </>
   );
 }
 
